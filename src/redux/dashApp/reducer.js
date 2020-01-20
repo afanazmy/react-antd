3037c67de75b695ce8c9644a66011532cf1a0ce5
ptksp-afan <ptksp.afan@gmail.com>
2020-01-20 13:31:42 +0700

    First Commit

---
+++ src/redux/dashApp/reducer.js
@@ -0,0 +1,10 @@
+const initState = {
+  language: "EN"
+};
+
+export default function dashAppReducer(state = initState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
